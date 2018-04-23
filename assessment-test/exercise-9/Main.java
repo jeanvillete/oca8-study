e5c038f53d452a28f507d50a269f18ad8aa20b37
Jean Villete <jean.villete@gmail.com>
2018-04-22 22:35:33 -0300

    Adding java code and notes for exercise 8

---
+++ assessment-test/exercise-9/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args){
+	}
+}
+
+/*
+* 1 - Checked exceptions are intended to be thrown by the JVM (and not the programmer) (False)
+* 2 - Checked exceptions are required to be caught or declared. (True)
+* 3 - Errors are intended to be thrown by the JVM (and not the programmer) (True)
+* 4 - Errors are required to be caught or declared. (False)
+* 5 - Runtime exceptions are intended to be thrown by the JVM (and not the programmer) (False)
+* 6 - Runtime exceptions are required to be caught or declared. (False)
+*/
