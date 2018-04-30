1b040026a2443ae3b60aa734aa64d0c85c1c6db5
Jean Villete <jean.villete@gmail.com>
2018-04-30 19:12:31 -0300

    Adding java code and notes for exercise 16

---
+++ assessment-test/exercise-16/Animal.java
@@ -0,0 +1,3 @@
+public interface Animal { public default String getName() { return null; } }
+interface Mammal { public default String getName() { return null; } }
+abstract class Otter implements Mammal, Animal {}
