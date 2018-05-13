c3c7a7bf5ef07a2e3be5d33d72736e6252fdd515
Jean Villete <jean.villete@gmail.com>
2018-05-13 11:50:31 -0300

    Correcting answer for exercise-19

--- chapter-1/exercise-19/Rabbit.java
+++ chapter-1/exercise-19/Rabbit.java
@@ -13,8 +13,8 @@ public class Rabbit {
 
 /*
 * 1/A - The Rabbit object from line 3 is first eligible for garbage collection immediately following line 6 (False)
-* 2/B - The Rabbit object from line 3 is first eligible for garbage collection immediately following line 8 (False)
-* 3/C - The Rabbit object from line 3 is first eligible for garbage collection immediately following line 12 (True)
+* 2/B - The Rabbit object from line 3 is first eligible for garbage collection immediately following line 8 (True)
+* 3/C - The Rabbit object from line 3 is first eligible for garbage collection immediately following line 12 (False)
 * 4/D - The Rabbit object from line 4 is first eligible for garbage collection immediately following line 9 (True)
 * 5/E - The Rabbit object from line 4 is first eligible for garbage collection immediately following line 11 (False)
 * 6/F - The Rabbit object from line 4 is first eligible for garbage collection immediately following line 12 (False)
